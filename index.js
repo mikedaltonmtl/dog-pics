a00750cd33b9e73b99593e3048ae992fea283f87
mike-dalton <michael.j.dalton@gmail.com>
2023-07-31 20:43:34 -0400

    add styles and js scripts

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ const PORT = 3000;
 
 const app = express();
 
+app.use(express.static("public"));
+
 app.get('/', (req, res) => {
-  res.json({ ok: true });
+  res.render('index', { text: "test data" });
 });
 
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
